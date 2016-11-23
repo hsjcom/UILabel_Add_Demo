1009fb1bc00e3a45d1b9b7f305fff340d7287613
Shaojie Hong <hsjcn@qq.com>
2016-11-23 15:26:12 +0800

    Initial Commit

---
+++ UILabel_Add_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILabel_Add_Demo
+//
+//  Created by Soldier on 2016/11/23.
+//  Copyright © 2016年 Shaojie Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UILabel_Add_Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UILabel_Add_Demo
+//
+//  Created by Soldier on 2016/11/23.
+//  Copyright © 2016年 Shaojie Hong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
