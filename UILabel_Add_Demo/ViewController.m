d2fbd141e716754a07cee84a62323d6c9de6e493
Shaojie Hong <hsjcn@qq.com>
2016-12-02 16:30:32 +0800

    background modify

--- UILabel_Add_Demo/ViewController.m
+++ UILabel_Add_Demo/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 #import "UILabel+Add.h"
 
+#define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]
+
 @interface ViewController ()
 
 @end
@@ -17,6 +19,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.view.backgroundColor = RGBACOLOR(250, 250, 250, 1);
     
     [self constructView];
 }
@@ -32,7 +36,7 @@
     label.keywords = @"困难于其易，为大于其细。天下难事必作于易，天下大事必于细。";
     label.keywordsFont = [UIFont boldSystemFontOfSize:20];
     label.keywordsColor = [UIColor orangeColor];
-    label.underlineStr = @"多";
+    label.underlineStr = @"夫轻诺必寡信，多易必多难。";
     label.underlineColor = [UIColor orangeColor];
     
     label.styleText = @"为无为，事无事，味无味。大小多少，报怨以德。困难于其易，为大于其细。天下难事必作于易，天下大事必于细。是以圣人终不为大，故能成其大。夫轻诺必寡信，多易必多难。是以圣人犹难之，故终无难矣。";
